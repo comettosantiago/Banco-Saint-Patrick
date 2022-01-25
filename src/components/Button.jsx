829c11dd4f8b5f919394eb16ca2ae452484b078f
Andu15 <estefania_8_3@hotmail.com>
2022-01-24 20:00:53 -0500

    fix: reorganize the structure of the home page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
-const Button = () => {
+const Button = ({ text, route }) => {
   return (
-    <div>
-      
+    <div className='container-btn'>
+      <Link type="button" className="btn main-btn" to={`/${route}`}>
+        {text}
+      </Link>
     </div>
   )
 }
 
-export default Button
+export default Button;
