9557db880ee6e7455e895f95f45769224c99f819
Andu15 <estefania_8_3@hotmail.com>
2022-01-27 19:04:22 -0500

    feat: add error page

---
+++ src/assets/utils/useForm.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+export const useForm = (initialForm, validateForm) => {
+  const [form, setForm] = useState(initialForm);
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+  // const [response, setResponse] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validateForm(form));
+
+    if(Object.keys(errors).length === 0){
+      alert('formulario enviado');
+      // setLoading(true);
+    } else {
+      alert('formulario NO enviado');
+      return;
+    }
+  }
+  
+  const handleBlur = (e) => {
+    handleChange(e);
+    setErrors(validateForm(form));
+    // handleSubmit(e);
+    if(form.dni.trim() && form.pin.trim()){
+      handleSubmit(e);
+    } else {
+      return;
+    }
+  }
+
+  return {
+    form,
+    errors,
+    loading,
+    // response,
+    handleChange,
+    handleBlur,
+    handleSubmit
+  }
+}
---
+++ src/pages/Error404.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Error404 = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <div class="cloak__wrapper">
+        <div class="cloak__container">
+          <div class="cloak"></div>
+        </div>
+      </div>
+      <div class="info">
+        <h2>Oops!</h2>
+        <p>No encontramos lo qu buscabas.</p><a href="https://jhey.dev" target="_blank" rel="noreferrer noopener">Volver al inicio</a>
+      </div>
+    </div>
+  )
+}
+
+export default Error404;
