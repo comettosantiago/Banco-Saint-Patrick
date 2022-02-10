3ca31445cf3b170ec70bec9d546cc29084eda05b
Andu15 <estefania_8_3@hotmail.com>
2022-02-10 16:48:51 -0500

    feat: main structure

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+
 import CreditCard from "../components/CreditCard";
 import { NavBar } from "../components/NavBar.jsx";
+import mainImg from "../assets/images/mainImag.png";
+import TransactionLabel from "../components/TransactionLabel";
 
 const Main = () => {
   return (
     <>
       <NavBar />
-      <CreditCard />
+
+      <section className="">
+        <div>
+          <div>
+            <h2>Hola, <span>Noelis</span></h2>
+            <p>¿Que haremos hoy?</p>
+          </div>
+          <img src={mainImg}/>
+        </div>
+
+        <p>Mis tarjetas</p>
+        <div>
+          <CreditCard/>
+        </div>
+
+      </section>
+
+      <section>
+        <h2>MOVIMIENTOS</h2>
+        <div>
+          <TransactionLabel/>
+        </div>
+      </section>
     </>
   );
 };
