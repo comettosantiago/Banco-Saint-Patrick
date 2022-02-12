3a4b51b42672f1e50eda47aaa128c4926d4ff519
Andu15 <estefania_8_3@hotmail.com>
2022-02-11 19:05:17 -0500

    feat: add favorite item structure

---
+++ src/components/FavoriteItems.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Icon } from '@iconify/react';
+
+import '../styles/FavoriteItems.css';
+
+const FavoriteItems = () => {
+  return (
+    <>
+      <section className='favorite-items-container'>
+        <p>Claudia</p>
+        <Icon icon="dashicons:arrow-up-alt2" color="#706e6e" height="20" rotate={1} />
+      </section>
+      <hr className='favorite-divider-line'/>
+    </>
+  )
+}
+
+export default FavoriteItems
--- src/pages/Transfer.jsx
+++ src/pages/Transfer.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Icon } from '@iconify/react';
 
 import NavTransfer from '../components/NavTransfer';
 
 const Transfer = () => {
   return (
-    <div>
+    <>
       <NavTransfer/>
-    </div>
+      {/* <FavoriteItems/> */}
+      <section className='container'>
+        <p>Elige a quién tranferir</p>
+        <button>Tranferir a otros<Icon icon="akar-icons:circle-plus" color="#f6b54d" height="20" rotate={1} /></button>
+        <div>
+          <p>Favoritos</p>
+          <hr/>
+        </div>
+      </section>
+    </>
   )
 }
 
