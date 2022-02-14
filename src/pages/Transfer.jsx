dbc33762588884fc7c7dbff23c703f8c1ea6c4c6
Andu15 <estefania_8_3@hotmail.com>
2022-02-14 16:20:08 -0500

    feat: add transfer form structure

--- src/pages/Transfer.jsx
+++ src/pages/Transfer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Icon } from '@iconify/react';
+import { Link } from "react-router-dom";
 
 import '../styles/Transfer.css';
 import NavTransfer from '../components/NavTransfer';
@@ -8,10 +9,10 @@ import FavoriteItems from '../components/FavoriteItems';
 const Transfer = () => {
   return (
     <>
-      <NavTransfer />
+      <NavTransfer text='Tranferir'/>
       <section className='containers'>
         <p className='title-text'>Elige a quién tranferir</p>
-        <button className='choose-transfer'>Tranferir a otros<Icon icon="akar-icons:circle-plus" color="#f6b54d" height="20" width="20" rotate={1} /></button>
+        <Link to="/transfer/process" className='choose-transfer'>Tranferir a otros<Icon icon="akar-icons:circle-plus" color="#f6b54d" height="20" width="20" rotate={1} /></Link>
         <div className='favorites-group'>
           <p className='favorites-title'>Favoritos</p>
         </div>
--- src/pages/TransferProcess.jsx
+++ src/pages/TransferProcess.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from 'react';
 
-const  TransferProcess = () => {
+import NavTransfer from '../components/NavTransfer';
+import Button from '../components/Button';
+
+const TransferProcess = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <NavTransfer text='Nueva transferencia' />
+      <section className='containers'>
+        <form>
+          <label
+            htmlFor='destinationAccount'
+          >Ingresa el numero de cuenta</label>
+          <input
+            id='destinationAccount'
+            type='text'
+            name='destinationAccount'
+          />
+
+          <label
+            htmlFor='rootAccount'
+          >Cuenta de origen</label>
+          <select
+            id='rootAccount'
+            name='rootAccount'
+          >
+            <option value="none"></option>
+            <option value="cuenta1">cuenta1</option>
+            <option value="cuenta2">cuenta2</option>
+          </select>
+
+          <label
+            htmlFor='reason'
+          >Motivo</label>
+          <input
+            id='reason'
+            type='text'
+            name='reason'
+          />
+
+          <label
+            htmlFor='amount'
+          >Monto</label>
+          <input
+            value='US$1.00'
+          />
+
+          <Button text='Continuar' route='transfer/confirm'/>
+          <Button text='Cancelar' route='main'/>
+        </form>
+      </section>
+    </>
   )
 }
 
-export default  TransferProcess
+export default TransferProcess
