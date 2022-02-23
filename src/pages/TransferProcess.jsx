d60554435939fea685339892d6c4e2e791238aed
CatherineChoque <tavita3c@gmail.com>
2022-02-23 16:35:52 -0500

    Estilos a la vista TranferProcess

--- src/pages/TransferProcess.jsx
+++ src/pages/TransferProcess.jsx
@@ -1,53 +1,37 @@
 import React from 'react';
 
 import NavTransfer from '../components/NavTransfer';
+import Button from './../components/Button';
 
 const TransferProcess = () => {
   return (
     <>
       <NavTransfer text='Nueva transferencia' />
       <section className='containers'>
-        <form>
-          <label
-            htmlFor='destinationAccount'
-          >Ingresa el numero de cuenta</label>
-          <input
-            id='destinationAccount'
-            type='text'
-            name='destinationAccount'
-          />
-
-          <label
-            htmlFor='rootAccount'
-          >Cuenta de origen</label>
-          <select
-            id='rootAccount'
-            name='rootAccount'
-          >
-            <option value="none"></option>
-            <option value="cuenta1">cuenta1</option>
-            <option value="cuenta2">cuenta2</option>
-          </select>
-
-          <label
-            htmlFor='reason'
-          >Motivo</label>
-          <input
-            id='reason'
-            type='text'
-            name='reason'
-          />
-
-          <label
-            htmlFor='amount'
-          >Monto</label>
-          <input
-            value='US$1.00'
-          />
-
-          {/* <Button text='Continuar' route='transfer/confirm'/>
-          <Button text='Cancelar' route='main'/> */}
+        <form className="form-detail">
+          <section className='transfer-detail'>
+            <p htmlFor='destinationAccount'>Ingresa el numero de cuenta</p>
+            <input id='destinationAccount' type='number' name='destinationAccount' placeholder='Número de cuenta'/>
+            <p htmlFor='rootAccount'>Cuenta de origen</p>
+            <select id='rootAccount' name='rootAccount'>
+              <option value="none">Elige una opcion</option>
+              <option value="cuenta1">cuenta1</option>
+              <option value="cuenta2">cuenta2</option>
+            </select>
+            <p htmlFor='reason'>Motivo</p>
+            <input id='reason' type='text' name='reason' placeholder='Escribe una nota' maxlength='30'/>
+          </section>
+          <section className='amount-detail'>
+            <p htmlFor='amount'>Monto</p>
+            <input value='US$1.00'/>
+          </section>
         </form>
+
+        <section className='btn-section'>
+          <Button text='Continuar' route='transfer/confirm'/>
+          <Button text='Cancelar' route='main' type='second-btn'/>
+        </section>
+
       </section>
     </>
   )
